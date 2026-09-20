1b783e6c0fc24f3e1f5239a3ae5def414d361c33
sachinkumar <sachinkumar5035@gmail.com>
2026-09-21 01:42:39 +0530

    date issue fixed

--- src/main/java/com/example/demo/dto/DepartmentDTO.java
+++ src/main/java/com/example/demo/dto/DepartmentDTO.java
@@ -2,10 +2,13 @@ package com.example.demo.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDate;
 
 @Getter
 @Setter
@@ -14,10 +17,11 @@ import lombok.Setter;
 public class DepartmentDTO {
 
     @NotBlank(message = "Name of the department cannot be blank")
-    @Size(min = 3, max = 10, message = "Number of characters in department name should be in the range: [3, 10]")
+    @Size(min = 3, max = 50, message = "Number of characters in department name should be in the range: [3, 10]")
     private String title;
 
     private Boolean isActive;
 
-    private String createdAt;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate createdAt;
 }
